d506e8dae641fe15ecd3e717226b61c74b67123e
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-07-24 22:33:41 +0300

    add AutoLoader

---
+++ AutoLoader.php
@@ -0,0 +1,11 @@
+<?php
+
+function AutoLoader($classname)
+{
+    $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace(('\\'), DIRECTORY_SEPARATOR, $classname) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+    spl_autoload_register('AutoLoader');
+}

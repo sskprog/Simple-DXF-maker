ad7baad3c9073618a906518969abeda3da8fa31f
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-07-26 23:10:31 +0300

    add Arc.php

---
+++ Entities/Arc.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Entities;
+
+class Arc extends Entity {
+
+    protected $data = [
+        'point' => [0,0],
+        'r' => 1,
+        'start' => 0,
+        'end' => 180
+    ];
+
+    function __toString()
+    {
+        $str = '0'.PHP_EOL. 'ARC'. PHP_EOL. '8'. PHP_EOL. '%s'.PHP_EOL.
+        $this->getOptionalProperties().
+        '10'.PHP_EOL. '%.2f'. PHP_EOL. '20'.PHP_EOL. '%.2f'.PHP_EOL.
+        '40'. PHP_EOL. '%.2f'.PHP_EOL. '50'. PHP_EOL. '%.2f'.PHP_EOL.
+        '51' . PHP_EOL . '%.2f' . PHP_EOL;
+
+        return sprintf(
+            $str,
+            $this->common['layer'],
+            $this->data['point'][0],
+            $this->data['point'][1],
+            $this->data['r'],
+            $this->data['start'],
+            $this->data['end']
+        );
+    }
+}

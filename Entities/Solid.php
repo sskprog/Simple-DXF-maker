4dc583be91bde48b92f236f4346bb1992f851e72
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-08-03 21:00:33 +0300

    edit __toString() in Solid.php

--- Entities/Solid.php
+++ Entities/Solid.php
@@ -14,11 +14,10 @@ class Solid extends Entity
         '10' . PHP_EOL . '%.2f' . PHP_EOL . '20' . PHP_EOL . '%.2f' . PHP_EOL .
         '11' . PHP_EOL . '%.2f' . PHP_EOL . '21' . PHP_EOL . '%.2f' . PHP_EOL .
         '12' . PHP_EOL . '%.2f' . PHP_EOL . '22' . PHP_EOL . '%.2f' . PHP_EOL;
-        if (isset($this->data['points'][3])) {
-            $str .= '13' . PHP_EOL . '%.2f' . PHP_EOL . '23' . PHP_EOL . '%.2f' . PHP_EOL;
-        }
 
-        return sprintf(
+        $end = '13' . PHP_EOL . '%.2f' . PHP_EOL . '23' . PHP_EOL . '%.2f' . PHP_EOL;
+
+        $result = sprintf(
             $str,
             $this->common['layer'],
             $this->data['points'][0][0],
@@ -27,8 +26,20 @@ class Solid extends Entity
             $this->data['points'][1][1],
             $this->data['points'][2][0],
             $this->data['points'][2][1],
-            $this->data['points'][3][0],
-            $this->data['points'][3][1],
         );
+        if (isset($this->data['points'][3])) {
+            $result .= sprintf(
+                $end,
+                $this->data['points'][3][0],
+                $this->data['points'][3][1],
+            );
+        } else {
+            $result .= sprintf(
+                $end,
+                $this->data['points'][2][0],
+                $this->data['points'][2][1],
+            );
+        }
+        return $result;
     }
 }

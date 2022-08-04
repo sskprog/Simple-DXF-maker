ce1dda39b63756d90fe0c981b675481ffc43cdd0
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-08-04 21:13:15 +0300

    add Style.php

---
+++ Face/Style.php
@@ -0,0 +1,37 @@
+<?php
+namespace Face;
+
+class Style extends Face
+{
+    protected $data = [
+        'height' => 0,
+        'width' => 1,
+        'angle' => 0,
+        'mirror' => 0,
+        'last' => 1,
+        'font' => 'SIMPLEX.SHX',
+        'bigfont' => null
+    ];
+
+    public function __toString()
+    {
+        $str = '0' . PHP_EOL . 'STYLE' . PHP_EOL . '2' . PHP_EOL . '%s' . PHP_EOL .
+                '70' . PHP_EOL . '%d' . PHP_EOL . '40' . PHP_EOL . '%.2f' . PHP_EOL .
+                '41' . PHP_EOL . '%.2f' . PHP_EOL . '50' . PHP_EOL . '%.2f' . PHP_EOL .
+                '71' . PHP_EOL . '%d' . PHP_EOL . '42' . PHP_EOL . '%.2f' . PHP_EOL .
+                '3' . PHP_EOL . '%s' . PHP_EOL . '4' . PHP_EOL . '%s' . PHP_EOL;
+
+        return sprintf(
+            $str,
+            $this->common['name'],
+            $this->common['flag'],
+            $this->data['height'],
+            $this->data['width'],
+            $this->data['angle'],
+            $this->data['mirror'],
+            $this->data['last'],
+            $this->data['font'],
+            $this->data['bigfont']
+        );
+    }
+}

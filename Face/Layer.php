273de793a709ac017b4737b09ed525bbd35ffa05
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-08-04 23:33:55 +0300

    add Layer.php

---
+++ Face/Layer.php
@@ -0,0 +1,31 @@
+<?php
+namespace Face;
+
+class Layer extends Face
+{
+    protected $data = [
+        'color' => 7
+    ];
+    protected $ltype;
+
+    public function __construct(array $properties, Ltype $ltype)
+    {
+        parent::__construct($properties);
+        $this->ltype = $ltype->getName();
+    }
+
+    public function __toString()
+    {
+        $str = '0' . PHP_EOL . 'LAYER' . PHP_EOL . '2' . PHP_EOL . '%s' . PHP_EOL .
+                '70' . PHP_EOL . '%d' . PHP_EOL . '62' . PHP_EOL . '%d' . PHP_EOL .
+                '6' . PHP_EOL . '%s' . PHP_EOL;
+
+        return sprintf(
+            $str,
+            $this->common['name'],
+            $this->common['flag'],
+            $this->data['color'],
+            $this->ltype
+        );
+    }
+}

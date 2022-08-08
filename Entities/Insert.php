b00e167c84266982ec64ce210442cda0e11c0dc4
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-08-08 21:18:27 +0300

    add Insert.php

---
+++ Entities/Insert.php
@@ -0,0 +1,39 @@
+<?php
+namespace Entities;
+
+class Insert extends Entity
+{
+    protected $data = [
+        'block' => null,
+        'point' => [0, 0],
+        'scale' => [1, 1],
+        'rotation' => 0,
+        'column' => [1, 0],
+        'row' => [1, 0]
+    ];
+
+    public function __toString()
+    {
+        $str = '0' . PHP_EOL . 'INSERT' . PHP_EOL . '8' . PHP_EOL . '%s' . PHP_EOL .
+                '2' . PHP_EOL . '%s' . PHP_EOL . '10' . PHP_EOL . '%.2f' . PHP_EOL .
+                '20' . PHP_EOL . '%.2f' . PHP_EOL . '41' . PHP_EOL . '%.2f' . PHP_EOL .
+                '42' . PHP_EOL . '%.2f' . PHP_EOL . '50' . PHP_EOL . '%.2f' . PHP_EOL .
+                '70' . PHP_EOL . '%.2f' . PHP_EOL . '44' . PHP_EOL . '%.2f' . PHP_EOL .
+                '71' . PHP_EOL . '%.2f' . PHP_EOL . '45' . PHP_EOL . '%.2f' . PHP_EOL;
+
+        return sprintf(
+            $str,
+            $this->common['layer'],
+            $this->data['block'],
+            $this->data['point'][0],
+            $this->data['point'][1],
+            $this->data['scale'][0],
+            $this->data['scale'][1],
+            $this->data['rotation'],
+            $this->data['column'][0],
+            $this->data['column'][1],
+            $this->data['row'][0],
+            $this->data['row'][1]
+        );
+    }
+}

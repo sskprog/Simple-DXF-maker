cfd4eded7df2893be25ba8321efb0316229e9124
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-08-08 22:20:20 +0300

    add Draw.php

---
+++ Base/Draw.php
@@ -0,0 +1,82 @@
+<?php
+namespace Base;
+
+use Face\Layer;
+use Face\Ltype;
+use Face\Style;
+
+class Draw
+{
+    protected $header;
+    protected $ltype;
+    protected $countLtype = 0;
+    protected $layer;
+    protected $countLayer = 0;
+    protected $style;
+    protected $countStyle = 0;
+    protected $block;
+
+    protected $headTable = '0' . PHP_EOL . 'TABLE' . PHP_EOL . '2' . PHP_EOL;
+    protected $endTable = '0' . PHP_EOL . 'ENDTAB' . PHP_EOL;
+    protected $headSection = '0' . PHP_EOL . 'SECTION' . PHP_EOL . '2' . PHP_EOL;
+    protected $endSection = '0' . PHP_EOL . 'ENDSEC' . PHP_EOL;
+
+    public function __construct()
+    {
+        $this->header = include 'Header.php';
+    }
+
+    public function addLtype(Ltype $ltype)
+    {
+        $this->ltype .= sprintf('%s', $ltype);
+        $this->countLtype++;
+    }
+
+    public function addLayer(Layer $layer)
+    {
+        $this->layer .= sprintf('%s', $layer);
+        $this->countLayer++;
+    }
+
+    public function addStyle(Style $style)
+    {
+        $this->style .= sprintf('%s', $style);
+        $this->countStyle++;
+    }
+
+    public function addBlock(Block $block)
+    {
+        $this->block .= sprintf('%s', $block);
+    }
+
+    public function __toString()
+    {
+        $str =
+                $this->headSection .
+                'HEADER' . PHP_EOL . $this->header .
+                $this->endSection .
+                $this->headSection .
+                'TABLES' . PHP_EOL .
+                $this->headTable .
+                'LTYPE' . PHP_EOL . '70' . PHP_EOL .
+                $this->countLtype . PHP_EOL .
+                $this->ltype .
+                $this->endTable .
+                $this->headTable .
+                'LAYER' . PHP_EOL . '70' . PHP_EOL .
+                $this->countLayer . PHP_EOL .
+                $this->layer .
+                $this->endTable .
+                $this->headTable .
+                'STYLE' . PHP_EOL . '70' . PHP_EOL .
+                $this->countStyle . PHP_EOL .
+                $this->style .
+                $this->endTable .
+                $this->endSection .
+                $this->headSection .
+                'BLOCKS' . PHP_EOL . $this->block .
+                $this->endSection;
+
+        return $str;
+    }
+}

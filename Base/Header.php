674627f76c078be682eba0a7c0a64f3e00cbc326
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-08-08 21:43:36 +0300

    add Header.php

---
+++ Base/Header.php
@@ -0,0 +1,15 @@
+<?php
+
+$header =
+            '9' . PHP_EOL . '$ACADVER' . PHP_EOL .
+            '1' . PHP_EOL . 'AC1009' . PHP_EOL .
+            '9' . PHP_EOL . '$INSBASE' . PHP_EOL .
+            '10' . PHP_EOL . '0.00' . PHP_EOL .
+            '20' . PHP_EOL . '0.00' . PHP_EOL .
+            '9' . PHP_EOL . '$EXTMIN' . PHP_EOL .
+            '10' . PHP_EOL . '0.00' . PHP_EOL .
+            '20' . PHP_EOL . '0.00' . PHP_EOL .
+            '9' . PHP_EOL . '$EXTMAX' . PHP_EOL .
+            '10' . PHP_EOL . '0.00' . PHP_EOL .
+            '20' . PHP_EOL . '0.00' . PHP_EOL;
+return $header;

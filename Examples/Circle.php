df3ae8ccc11692cf13992fd4e7eca42437efdf19
Stanislav Kushnir <stas.kushnir@gmail.com>
2022-09-25 13:45:52 +0300

    edit comment in example Circle.php

--- Examples/Circle.php
+++ Examples/Circle.php
@@ -21,4 +21,4 @@ $draw->save('circle');
 
 //Default charset is 'UTF-8'. If you want to use a different encoding then add
 //the second parameter to the method. For example:
-//$draw->save('circle', 'Windows-1251);
+//$draw->save('circle', 'Windows-1251');
